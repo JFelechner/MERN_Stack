f04239b8a699c9b36af0136e654bea7e4bed0388
JFelechner <jfelechner@comcast.net>
2022-04-25 23:08:54 -0500

    updates

--- project_week/personal_project/felechner_mern_project/client/src/App.jsx
+++ project_week/personal_project/felechner_mern_project/client/src/App.jsx
@@ -21,9 +21,9 @@ function App() {
             <SignIn></SignIn>
           </Route>
 
-          <Route exact path="/dashboard">
+          {/* <Route exact path="/dashboard">
             <Dashboard></Dashboard>
-          </Route>
+          </Route> */}
 
           <Route exact path="/game_acheievments">
             <GameAchievements></GameAchievements>
@@ -32,10 +32,10 @@ function App() {
           {/* Below is URL to leverage microsoft azur login */}
           {/*  Public URL --> https://xbl.io/app/auth/6d78d895-23e6-4de4-afa4-6dd76a8bf367  */}
           {/* 
+        */}
           <Route exact path="/auth/grant">
             <Dashboard></Dashboard>
           </Route>  
-          */}
 
         </Switch>
 
